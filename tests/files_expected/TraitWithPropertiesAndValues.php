38bad3c2d6233bd20a1de0860787adb92853c5f4
Mostafa Ghoneimy <emostafagh@gmail.com>
2019-02-02 19:28:44 +0200

    GQL-8: Added tests for TraitFile and AbstractCodeFile classes - Added tests for file writing - Added tests for empty trait generation - Added tests for properties generation

---
+++ tests/files_expected/TraitWithPropertiesAndValues.php
@@ -0,0 +1,11 @@
+<?php
+
+trait TraitWithPropertiesAndValues
+{
+
+    protected $propertyOne;
+    protected $propertyTwo = 2;
+    protected $propertyThree = 'three';
+    protected $propertyFour = false;
+    protected $propertyFive = true;
+}
