68f6701eddc107e848ea653d413096214acf6cd1
Mostafa Ghoneimy <mostafa@zlien.com>
2019-02-23 16:33:50 +0200

    GQL-14: Completed unit test for EnumObjectBuilder

---
+++ tests/files_expected/enum_objects/EmptyEnumObject.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace GraphQL\SchemaObject;
+
+class EmptyEnumObject extends EnumObject
+{}
---
+++ tests/files_expected/enum_objects/WithMultipleConstantsEnumObject.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GraphQL\SchemaObject;
+
+class WithMultipleConstantsEnumObject extends EnumObject
+{
+    const SOME_VALUE = 'some_value';
+    const ANOTHER_VALUE = 'another_value';
+    const ONE_MORE_VALUE = 'one_more_value';
+}
