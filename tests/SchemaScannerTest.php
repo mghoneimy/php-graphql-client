2921568fc1d645eb22cf831ad1fc5a9390ca8c45
Mostafa Ghoneimy <mostafa@zlien.com>
2019-03-02 01:58:40 +0200

    GQL-14: Completed tests for SchemaScanner class to generate input and enum objects - Completed tests for generating input and query objects classes from schemas with input objects - Completed tests for generating enum and query objects classes from schema with enum values - Fixed issue in write dir in schema scanner - Added more tests to test the functionality of generating arguments with list values

--- tests/SchemaScannerTest.php
+++ tests/SchemaScannerTest.php
@@ -23,22 +23,24 @@ class SchemaScannerTest extends CodeFileTestCase
     }
 
     /**
-     * @param array  $schemaTypes
-     * @param string $expectedFileName
+     * @param array $schemaTypes
+     * @param array $expectedFileNames
      *
      * @dataProvider schemaStringProvider
      * 
      * @covers \GraphQL\SchemaGenerator\SchemaScanner::generateSchemaObjects
      */
-    public function testSchemaTypesReading(array $schemaTypes, $expectedFileName)
+    public function testSchemaTypesReading(array $schemaTypes, array $expectedFileNames)
     {
         $schemaScanner = new SchemaScanner();
         $schemaScanner->generateSchemaObjects($schemaTypes, static::getGeneratedFilesDir());
 
-        $this->assertFileEquals(
-            static::getExpectedFilesDir() . "/$expectedFileName.php",
-            static::getGeneratedFilesDir() . "/$expectedFileName.php"
-        );
+        foreach ($expectedFileNames as $expectedFileName) {
+            $this->assertFileEquals(
+                static::getExpectedFilesDir() . "/$expectedFileName.php",
+                static::getGeneratedFilesDir() . "/$expectedFileName.php"
+            );
+        }
     }
 
     /**
@@ -97,8 +99,22 @@ class SchemaScannerTest extends CodeFileTestCase
                             'inputFields' => null,
                             'ofType' => null,
                         ]
-                    ],
-                    [
+                    ], [
+                        'name' => '_ids',
+                        'description' => '',
+                        'type' => [
+                            'name' => null,
+                            'description' => null,
+                            'kind' => 'LIST',
+                            'inputFields' => null,
+                            'ofType' => [
+                                'name' => 'String',
+                                'description' => 'Build-in String',
+                                'kind' => 'SCALAR',
+                                'enumValues' => null
+                            ],
+                        ]
+                    ], [
                         'name' => 'name',
                         'description' => '',
                         'type' => [
@@ -108,8 +124,22 @@ class SchemaScannerTest extends CodeFileTestCase
                             'inputFields' => null,
                             'ofType' => null,
                         ]
-                    ],
-                    [
+                    ], [
+                        'name' => 'names',
+                        'description' => '',
+                        'type' => [
+                            'name' => null,
+                            'description' => null,
+                            'kind' => 'LIST',
+                            'inputFields' => null,
+                            'ofType' => [
+                                'name' => 'String',
+                                'description' => 'Build-in String',
+                                'kind' => 'SCALAR',
+                                'enumValues' => null
+                            ],
+                        ]
+                    ], [
                         'name' => 'creation_date',
                         'description' => '',
                         'type' => [
@@ -194,9 +224,226 @@ class SchemaScannerTest extends CodeFileTestCase
             ],
         ];
 
+        $orderingObjectName = '_Ordering';
+        $withEnumsObjectSchema = [
+            [
+                'name' => 'WithEnum',
+                'description' => '',
+                'type' => [
+                    'name' => null,
+                    'kind' => 'LIST',
+                    'ofType' => [
+                        'name' => 'WithEnum',
+                        'kind' => 'OBJECT',
+                        'description' => 'some description',
+                        'fields' =>[
+                            [
+                                'name' => '_id',
+                                'description' => 'object id',
+                                'type' => [
+                                    'name' => 'Long',
+                                    'kind' => 'SCALAR',
+                                    'ofType' => null
+                                ]
+                            ], [
+                                'name' => 'creation_date',
+                                'description' => 'object creation date',
+                                'type' => [
+                                    'name' => 'String',
+                                    'kind' => 'SCALAR',
+                                    'ofType' => null
+                                ]
+                            ]
+                        ],
+                    ],
+                ],
+                'args' => [
+                    [
+                        'name' => '_id',
+                        'description' => '',
+                        'type' => [
+                            'name' => 'LONG',
+                            'description' => 'Long type',
+                            'kind' => 'SCALAR',
+                            'inputFields' => null,
+                            'ofType' => null,
+                        ]
+                    ],
+                    [
+                        'name' => 'creation_date',
+                        'description' => '',
+                        'type' => [
+                            'name' => 'String',
+                            'description' => 'Built-in String',
+                            'kind' => 'SCALAR',
+                            'inputFields' => null,
+                            'ofType' => null,
+                        ]
+                    ],
+                    [
+                        'name' => 'orderBy',
+                        'description' => null,
+                        'type' => [
+                            'name' => null,
+                            'description' => null,
+                            'kind' => 'LIST',
+                            'inputFields' => null,
+                            'ofType' => [
+                                'name' => $orderingObjectName,
+                                'description' => 'ordering of this object',
+                                'kind' => 'ENUM',
+                                'enumValues' => [
+                                    [
+                                        'name' => 'creation_date_asc',
+                                        'description' => 'Ascending order of creation date',
+                                    ],
+                                    [
+                                        'name' => 'creation_date_desc',
+                                        'description' => 'Descending order of creation date',
+                                    ],
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+            ],
+        ];
+
+        $inputObjectName = '_FilterBy';
+        $withInputObjectSchema = [
+            [
+                'name' => 'WithInput',
+                'description' => '',
+                'type' => [
+                    'name' => null,
+                    'kind' => 'LIST',
+                    'ofType' => [
+                        'name' => 'WithInput',
+                        'kind' => 'OBJECT',
+                        'description' => 'some description',
+                        'fields' =>[
+                            [
+                                'name' => '_id',
+                                'description' => 'object id',
+                                'type' => [
+                                    'name' => 'Long',
+                                    'kind' => 'SCALAR',
+                                    'ofType' => null
+                                ]
+                            ], [
+                                'name' => 'name',
+                                'description' => 'object name',
+                                'type' => [
+                                    'name' => 'String',
+                                    'kind' => 'SCALAR',
+                                    'ofType' => null
+                                ]
+                            ]
+                        ],
+                    ],
+                ],
+                'args' => [
+                    [
+                        'name' => '_id',
+                        'description' => '',
+                        'type' => [
+                            'name' => 'LONG',
+                            'description' => 'Long type',
+                            'kind' => 'SCALAR',
+                            'inputFields' => null,
+                            'ofType' => null,
+                        ]
+                    ], [
+                        'name' => 'name',
+                        'description' => '',
+                        'type' => [
+                            'name' => 'String',
+                            'description' => 'Build-in String',
+                            'kind' => 'SCALAR',
+                            'inputFields' => null,
+                            'ofType' => null,
+                        ]
+                    ], [
+                        'name' => 'filterBy',
+                        'description' => '',
+                        'type' => [
+                            'name' => $inputObjectName,
+                            'description' => 'Filter input object',
+                            'kind' => 'INPUT_OBJECT',
+                            'inputFields' => [
+                                [
+                                    'name' => 'name',
+                                    'description' => '',
+                                    'type' => [
+                                        'name' => 'String',
+                                        'description' => '',
+                                        'kind' => 'SCALAR',
+                                        'ofType' => null,
+                                    ]
+                                ], [
+                                    'name' => 'name_contains',
+                                    'description' => '',
+                                    'type' => [
+                                        'name' => 'String',
+                                        'description' => '',
+                                        'kind' => 'SCALAR',
+                                        'ofType' => null,
+                                    ]
+                                ], [
+                                    'name' => 'name_not',
+                                    'description' => '',
+                                    'type' => [
+                                        'name' => 'String',
+                                        'description' => '',
+                                        'kind' => 'SCALAR',
+                                        'ofType' => null,
+                                    ]
+                                ], [
+                                    'name' => 'name_in',
+                                    'description' => '',
+                                    'type' => [
+                                        'name' => null,
+                                        'description' => '',
+                                        'kind' => 'LIST',
+                                        'ofType' => [
+                                            'name' => 'String',
+                                            'kind' => 'SCALAR',
+                                            'description' => '',
+                                            'ofType' => null,
+                                        ],
+                                    ]
+                                ], [
+                                    'name' => 'name_not_in',
+                                    'description' => '',
+                                    'type' => [
+                                        'name' => null,
+                                        'description' => '',
+                                        'kind' => 'LIST',
+                                        'ofType' => [
+                                            'name' => null,
+                                            'description' => '',
+                                            'kind' => 'NOT_NULL',
+                                            'ofType' => [
+                                                'name' => 'String',
+                                                'description' => '',
+                                                'kind' => 'SCALAR',
+                                            ]
+                                        ],
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'ofType' => null,
+                    ]
+                ]
+            ],
+        ];
+
         return [
-            'SimpleObjectCase' => [$simpleObjectSchema, 'SimpleObjectQueryObject'],
-            'ComplexObjectCase' => [$complexObjectSchema, 'ComplexObjectQueryObject'],
+            'SimpleObjectCase' => [$simpleObjectSchema, ['SimpleObjectQueryObject']],
+            'ComplexObjectCase' => [$complexObjectSchema, ['ComplexObjectQueryObject']],
+            'WithEnumCase' => [$withEnumsObjectSchema, ['WithEnumQueryObject', $orderingObjectName . 'EnumObject']],
+            'WithInputObjectCase' => [$withInputObjectSchema, ['WithInputQueryObject', $inputObjectName . 'InputObject']],
         ];
     }
 }
