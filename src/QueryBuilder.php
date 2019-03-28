257a8b2ad26def36dfd18d2d271a2114bd5e86ff
Mostafa Ghoneimy <emostafagh@gmail.com>
2019-03-28 02:05:27 +0200

    GQL-27: Restructured the code to consolidate the logic building a query in one class - Moved query building logic to AbstractQueryBuilder class - Refactored the QueryBuilder extend the AbstractQueryBuilder and changed some its methods visibility - Refactored the QueryObject class to extend the AbstractQueryBuilder class

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -2,91 +2,53 @@
 
 namespace GraphQL;
 
-use GraphQL\Exception\EmptySelectionSetException;
-
 /**
  * Class QueryBuilder
  *
  * @package GraphQL
  */
-class QueryBuilder
+class QueryBuilder extends AbstractQueryBuilder
 {
     /**
-     * @var string
-     */
-    protected $queryObject;
-
-    /**
-     * @var array
-     */
-    protected $selectionSet;
-
-    /**
-     * @var array
-     */
-    protected $argumentsList;
-
-    /**
-     * QueryBuilder constructor.
+     * Changing method visibility to public
      *
-     * @param string $queryObject
-     */
-    public function __construct(string $queryObject)
-    {
-        $this->queryObject   = $queryObject;
-        $this->selectionSet  = [];
-        $this->argumentsList = [];
-    }
-
-    /**
-     * @param string|QueryBuilder|Query $selectedField
+     * @param Query|QueryBuilder|string $selectedField
      *
-     * @return $this
+     * @return AbstractQueryBuilder|QueryBuilder
      */
     public function selectField($selectedField)
     {
-        if (is_string($selectedField) || $selectedField instanceof QueryBuilder || $selectedField instanceof Query) {
-            $this->selectionSet[] = $selectedField;
-        }
-
-        return $this;
+        return parent::selectField($selectedField);
     }
 
     /**
-     * @param $argumentName
-     * @param $argumentValue
+     * Changing method visibility to public
      *
-     * @return $this
+     * @param string $argumentName
+     * @param        $argumentValue
+     *
+     * @return AbstractQueryBuilder
      */
     public function setArgument(string $argumentName, $argumentValue)
     {
-        if (is_scalar($argumentValue) || is_array($argumentValue) || $argumentValue instanceof RawObject) {
-            $this->argumentsList[$argumentName] = $argumentValue;
-        }
-
-        return $this;
+        return parent::setArgument($argumentName, $argumentValue);
     }
 
     /**
+     * Changing method visibility to public
+     *
      * @return Query
      */
     public function toQuery(): Query
     {
-        if (empty($this->selectionSet)) {
-            throw new EmptySelectionSetException(static::class);
-        }
+        return parent::toQuery();
+    }
 
-        // Convert nested query builders to query objects
-        foreach ($this->selectionSet as $key => $field) {
-            if ($field instanceof QueryBuilder) {
-                $this->selectionSet[$key] = $field->toQuery();
-            }
-        }
-
-        $query = new Query($this->queryObject);
-        $query->setArguments($this->argumentsList);
-        $query->setSelectionSet($this->selectionSet);
-
-        return $query;
+    /**
+     * @return array
+     */
+    protected function constructArgumentsList(): array
+    {
+        return [];
     }
 }
